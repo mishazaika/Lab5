4815e64688382b58036dcfc55c340dca7224ecf9
MikhailZaika <mishazaika789@gmail.com>
2020-04-07 21:36:52 +0300

    Lab5

---
+++ Lab5/Lab5/Lab5(2)/Faculty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab5.Lab5_2_
+{
+    class Faculty
+    {
+
+
+
+
+        public class Department
+        {
+
+        }
+    }
+}
---
+++ Lab5/Lab5/Lab5(2)/Teacher.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab5.Lab5_2_
+{
+    class Teacher
+    {
+    }
+}
