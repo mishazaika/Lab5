87a0a3e676e342247804cf9c269279ce395955a5
MikhailZaika <mishazaika789@gmail.com>
2020-04-07 21:30:50 +0300

    Lab5(1)

---
+++ Lab5/Lab5/St_Assesment.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab5
+{
+    class St_Assesment
+    {
+        private Random random = new Random();
+        private int[,] arrRating = new int[9, 9];
+        private int sum;
+        private int k;
+
+        public St_Assesment()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            sum = 0;
+            k = 81;
+        }
+
+        public void StRating()
+        {
+            for(int i = 0; i< 9;  i++)
+            {
+                for(int j = 0; j < 9; j++)
+                {
+                    arrRating[i, j] = random.Next(46, 100);
+                }
+            }
+        }
+
+        public void MyRating()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+
+                    Console.Write(arrRating[i, j] + " ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("__________________________");
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+
+                    sum += arrRating[i, j];
+            }
+            sum = sum / k;
+            Console.WriteLine("Середній рейтинг за семестр: " + Convert.ToString(sum));
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Lab5/Lab5/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab5
+{
+    class Student
+    {
+        static void Main(string[] args)
+        {
+            St_Assesment straight1 = new St_Assesment();
+            St_Assesment straight2 = new St_Assesment();
+
+            Console.WriteLine("\tПерший семестр\t");
+            Console.WriteLine("__________________________");
+            straight1.StRating();
+            straight1.MyRating();
+
+            Console.WriteLine("\tДругий семестр\t");
+            Console.WriteLine("__________________________");
+            straight2.StRating();
+            straight2.MyRating();
+
+        }
+
+        
+    }
+}
